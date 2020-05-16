de91438d5c56d63222bb091ef7326afec179f10e
Sanyi <sanyi@picon.local>
2020-05-16 14:59:25 +0200

    struct - teljes program - korok metszese

---
+++ ea20200516_6.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/* Korok metszespontja feladat */
+
+struct Kor
+{
+	float x,y,rad;
+};
+
+
+void Fill_Kor(struct Kor*);
+void Print_Kor(struct Kor);
+
+int Calc_Kor(struct Kor, struct Kor);
+	
+int main()
+{
+		struct Kor A,B;
+		Fill_Kor(&A);
+		Fill_Kor(&B);
+
+		Print_Kor(B);
+		printf("A ket kor metszi egymast: %d\n", Calc_Kor(A,B));
+
+		system("PAUSE");
+}
+
+
+void Fill_Kor(struct Kor* C)
+{
+		printf("Kerem a kor:x,y ");
+		scanf("%f,%f", &C->x, &C->y);
+		printf("\nKerem a kor:radius ");
+		scanf("%f", &C->rad);
+}
+
+void Print_Kor(struct Kor C)
+{
+		printf("Kor: x=%f, y=%f, rad=%f\n", C.x, C.y, C.rad);
+}
+
+
+int Calc_Kor(struct Kor C, struct Kor D)
+{
+		//0 ha nem metszi, 1 ha metszi
+		float tav, sumRad;
+		tav = sqrt(pow(C.x - D.x, 2) + pow(C.y - D.y,2));
+		sumRad = C.rad + D.rad;
+		if (tav> sumRad) return 0;
+		return 1;
+}
